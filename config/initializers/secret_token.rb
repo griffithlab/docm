cbe4017bd15a73485a429506ea1ebbdd4b51bcb8
Adam Coffman <acoffman@genome.wustl.edu>
2014-02-21 17:05:22 -0600

    empty rails app for docm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Docm::Application.config.secret_key_base = 'c2e0d62361fda856fe41755366fca77cfc6c02d3ed1034f551107e4e38a31e3556ff2df59947ce25edd7be00f281bea8b6617d42213353ef492cce0275da4a4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Docm::Application.config.session_store :cookie_store, key: '_docm_session'
