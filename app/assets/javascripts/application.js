4f4456f2e8a0080a7f4d3a6e021a8e402b53f1c2
Adam Coffman <acoffman@genome.wustl.edu>
2014-02-25 14:33:20 -0600

    setup jquery datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require dataTables/jquery.dataTables
+//= require dataTables/jquery.dataTables.bootstrap3
 //= require_tree .
