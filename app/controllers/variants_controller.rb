ab625c8c8234417eaeaf1bd9f771f919562cb6b6
Adam Coffman <acoffman@genome.wustl.edu>
2014-03-10 15:52:57 -0500

    only populate filters when html is requested

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -1,15 +1,19 @@
 class VariantsController < ApplicationController
   def index
-    @filters = Filter.registered_filters.map do |filter_class|
+    respond_to do |format|
+      format.html { @filters = get_filters }
+      format.json { render json: VariantsDatatable.new(view_context) }
+    end
+  end
+
+  private
+  def get_filters
+    Filter.registered_filters.map do |filter_class|
       {
         id: filter_class.param_name,
         label_text: filter_class.param_name.to_s.titleize,
         collection: filter_class.valid_values
       }
-    end
-    respond_to do |format|
-      format.html
-      format.json { render json: VariantsDatatable.new(view_context) }
     end
   end
 end
