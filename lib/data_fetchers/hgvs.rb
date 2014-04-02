00e8927089ede2733682bde17f8aec2a9315ff79
Adam Coffman <acoffman@genome.wustl.edu>
2014-04-02 11:20:53 -0500

    add support for other mutation type hgvs

--- lib/data_fetchers/hgvs.rb
+++ lib/data_fetchers/hgvs.rb
@@ -11,10 +11,32 @@ module DataFetchers
 
     #{transcript_name}:{c_position}{reference}>{variant}
     def self.get_hgvs_from_variant(variant)
+      send("#{variant.variant_type.name.downcase}_template", variant)
+    end
+
+    def self.snp_template(variant)
       sprintf("%s:%s%s>%s",
               variant.transcript_name,
               variant.cdna_change,
               variant.location.reference_read,
+              variant.variant)
+    end
+
+    def self.dnp_template(variant)
+      snp_template(variant)
+    end
+
+    def self.del_template(variant)
+      sprintf("%s:%sdel%s",
+              variant.transcript_name,
+              variant.cdna_change,
+              variant.location.reference_read)
+    end
+
+    def self.ins_template(variant)
+      sprintf("%s:%sins%s",
+              variant.transcript_name,
+              variant.cdna_change,
               variant.variant)
     end
   end
