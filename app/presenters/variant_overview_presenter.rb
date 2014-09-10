d424b5ffa73c3ef018f4958fefbac4ff60756ebc
Adam Coffman <acoffman@genome.wustl.edu>
2014-09-10 17:30:12 -0500

    add view support for drug interaction information

--- app/presenters/variant_overview_presenter.rb
+++ app/presenters/variant_overview_presenter.rb
@@ -11,6 +11,10 @@ class VariantOverviewPresenter < SimpleDelegator
     gene_link(gene)
   end
 
+  def my_cancer_genome_row
+    @view_context.content_tag(:li, my_cancer_genome_link(@variant))
+  end
+
   def disease_rows
     disease_source_variants.map do |dsv|
       DiseaseRow.new(
@@ -33,7 +37,21 @@ class VariantOverviewPresenter < SimpleDelegator
       )
     end
   end
+
+  def interaction_rows
+    drug_interactions.map do |di|
+      InteractionRow.new(
+        di.effect,
+        di.pathway,
+        di.therapeutic_context,
+        di.status,
+        di.evidence,
+        full_source_link(di)
+      )
+    end
+  end
 end
 
 DiseaseRow = Struct.new(:disease, :doid, :source)
 PermutationRow = Struct.new(:hgvs, :chromosome, :start, :stop, :reference, :variant)
+InteractionRow = Struct.new(:effect, :pathway, :drug, :status, :evidence_type, :source)
