4e2c4357a071758c9173c595b7a3260f27901066
Adam Coffman <acoffman@genome.wustl.edu>
2014-09-24 17:37:54 -0500

    avoid duplicate entites for the same source

--- lib/importers/row_adaptors/drug_interaction.rb
+++ lib/importers/row_adaptors/drug_interaction.rb
@@ -4,9 +4,13 @@ module Importers
       def self.create_from_row(row)
         1.upto(6).map do |i|
           map = column_map(i)
-          next if map["Association_#{i}"].blank?
-          ::DrugInteraction.where(get_property_hash_from_row(row, map))
-            .first_or_create
+          property_hash = get_property_hash_from_row(row, map)
+          next if property_hash['effect'].blank? || property_hash['pubmed_id'].blank? || property_hash['pubmed_id'].to_i == 0
+          pubmed_id =  property_hash.delete('pubmed_id')
+          source = ::Source.where(pubmed_id: pubmed_id.to_i).first_or_create
+          ::DrugInteraction.where(property_hash, source: source).first_or_create.tap do |di|
+            di.source = source
+          end
         end.compact
       end
 
