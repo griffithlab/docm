a505fe19f67f153540a442f60ecb730f07a179f0
Adam Coffman <acoffman@genome.wustl.edu>
2014-09-25 13:31:05 -0500

    truncate tables instead of dropping

--- lib/database/upgrade.rb
+++ lib/database/upgrade.rb
@@ -6,14 +6,20 @@ module Database
 
       if data_dump_version > current_database_version
         puts "Database out of date - loading the new data dump."
-        Rake::Task['db:drop'].execute
-        Rake::Task['db:create'].execute
-        Rake::Task['db:schema:load'].execute
+        Rake::Task['db:migrate'].execute
+        truncate_tables
         Database::Load.run
         DataVersion.create(version: data_dump_version)
         puts "Database upgraded to v#{data_dump_version}."
       else
         puts "Database v#{data_dump_version} is up to date."
+      end
+    end
+
+    private
+    def self.truncate_tables
+      ActiveRecord::Base.connection.tables.each do |table|
+        ActiveRecord::Base.connection.execute("TRUNCATE \"#{table}\" CASCADE;")
       end
     end
   end
