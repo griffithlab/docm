898e40fe0d48b9c1a2a9458810066caa6b23f65c
Adam Coffman <acoffman@genome.wustl.edu>
2015-06-17 10:46:27 -0500

    add new genes controller

---
+++ app/controllers/genes_controller.rb
@@ -0,0 +1,15 @@
+class GenesController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:index]
+
+  def variant_index
+    gene = params.delete(:name)
+    if Gene.where(name: gene).exists?
+      params[:genes] = [gene]
+      @multiselect_filters = get_multiselect_filters
+      @range_filters = get_range_filters
+      render 'variants/index'
+    else
+      head :not_found
+    end
+  end
+end
