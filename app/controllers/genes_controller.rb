85784063dabc55bfc5fcdacca71bb9bc674575b8
Adam Coffman <acoffman@genome.wustl.edu>
2015-06-17 12:36:53 -0500

    make gene name in path case insensitive

--- app/controllers/genes_controller.rb
+++ app/controllers/genes_controller.rb
@@ -3,9 +3,9 @@ class GenesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:index]
 
   def variant_index
-    gene = params.delete(:name)
-    if Gene.where(name: gene).exists?
-      params[:genes] = [gene]
+    gene_name = params.delete(:name).downcase
+    if gene = Gene.where('lower(genes.name) = :gene_name', gene_name: gene_name).first
+      params[:genes] = [gene.name]
       @multiselect_filters = get_multiselect_filters
       @range_filters = get_range_filters
       render 'variants/index'
