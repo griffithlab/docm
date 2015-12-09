350accb55408934745405d7befe283e6cd8f20b6
Adam Coffman <acoffman@genome.wustl.edu>
2015-12-09 13:23:49 -0600

    add publication filter

---
+++ app/filters/publication_filter.rb
@@ -0,0 +1,30 @@
+class PublicationFilter
+  include Filter
+
+  def self.filter(relation, values)
+    pubmed_extractor = Regexp.new(/.+ \((?<pubmed_id>\d+)\)$/)
+    pubmed_ids = values.map do |v|
+      if match_data = pubmed_extractor.match(v)
+        match_data[:pubmed_id].to_i
+      else
+        nil
+      end
+    end
+
+    relation.where('sources.pubmed_id' => pubmed_ids.compact)
+  end
+
+  def self.valid_values
+    Source.uniq
+      .order(:citation)
+      .map { |s| "#{s.citation.truncate(25, separator: ',')} (#{s.pubmed_id})" }
+  end
+
+  def self.param_name
+    :sources
+  end
+
+  def self.filter_type
+    :multiselect
+  end
+end
