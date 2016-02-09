2f08680647aaa058936314a5f1f80a4e5203b031
Adam Coffman <acc@fastmail.com>
2016-02-09 11:33:14 -0600

    Handle new JSON meta field correctly
    The proper drug interactions were not being pulled out of the
    new import format. Fixes #32

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203214847) do
+ActiveRecord::Schema.define(version: 20160209164657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160203214847) do
     t.string  "evidence"
     t.integer "variant_id"
     t.integer "source_id"
+    t.text    "clinical_association"
   end
 
   create_table "genes", force: :cascade do |t|
