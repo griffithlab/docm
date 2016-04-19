e507ad98e7479ea1bad36c2f91e155e2f960b488
Adam Coffman <acoffman@genome.wustl.edu>
2016-04-19 14:11:50 -0500

    add database index for tag name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419175927) do
+ActiveRecord::Schema.define(version: 20160419191118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(version: 20160419175927) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
   create_table "tags_variants", id: false, force: :cascade do |t|
     t.integer "tag_id",     null: false
