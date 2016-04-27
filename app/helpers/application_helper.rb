eeed9a5c520e10a93131f46868ea45bb05fd76a8
Adam Coffman <acoffman@genome.wustl.edu>
2016-04-27 11:01:19 -0500

    update link helper to point to batch submission location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def variant_submission_link(text = "Submit New Variant")
-    link_to(text, variant_submission_path, class: 'btn btn-primary')
+  def variant_submission_link(text = "Submit New Variants")
+    link_to(text, new_batch_path, class: 'btn btn-primary')
   end
 end
