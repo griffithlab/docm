875f7c32e4309957253a74d1543ef101e39d1ac8
Adam Coffman <acoffman@genome.wustl.edu>
2016-05-06 11:28:48 -0500

    move versioning logic out of util

--- app/models/version.rb
+++ app/models/version.rb
@@ -1,7 +1,31 @@
 class Version < ActiveRecord::Base
-  has_many :variants
+  has_many :disease_source_variants
 
   def self.current_version
-    self.where(is_current: true).first!
+    self.where(is_current: true).first || self.create(name: '0', is_current: true)
+  end
+
+  def self.create_new_version(update_type = :minor)
+    curr = current_version
+    version_string = curr.name
+    new_string = if version_string.include?('.')
+                  (major, minor) = version_string.split('.')
+                  if update_type == :minor
+                    [major, minor.next].join('.')
+                  else
+                    major.next
+                  end
+                 elsif update_type == :minor
+                   version_string + ".1"
+                 else
+                   version_string.next
+                 end
+    if self.find_by(name: new_string).present?
+      raise StandardError.new("Version with name #{new_string} already exists!")
+    else
+      curr.is_current = false
+      curr.save
+      Version.create(name: new_string, is_current: true)
+    end
   end
 end
