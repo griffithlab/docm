d89a8aab50b71ed31b4c0f574cff6fa8078aacb4
Adam Coffman <acoffman@genome.wustl.edu>
2016-05-06 11:29:58 -0500

    inital views for batches and moderation

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,4 +1,9 @@
 class BatchesController < ApplicationController
+
+  http_basic_authenticate_with name: 'mgi_user',
+    password: 'secret',
+    only: [:moderation_index, :show_moderation, :update_batch, :create_version]
+
   def new
     @batch = Batch.new
   end
@@ -8,13 +13,20 @@ class BatchesController < ApplicationController
 
     if @batch.save
       #SubmissionMailer.submission(@batch).deliver_now
-      ProcessBatchSubmission.new.perform(@batch)
+      ProcessBatchSubmission.perform_later(@batch)
       flash[:success] = 'Thank you! Your variants have been submitted successfully and we will be in touch shortly.'
       redirect_to private_batch_url(@batch.url_slug)
     else
       flash[:error] = 'There appears to be a problem with your submission'
       render :new
     end
+  end
+
+  def create_version
+    CreateNewVersion.perform_later
+    flash[:success] = 'New version is being processed.'
+    @batches = Batch.all
+    render :moderation_index
   end
 
   def show
@@ -25,8 +37,37 @@ class BatchesController < ApplicationController
     @batch = Batch.find_by(url_slug: params[:url_slug])
   end
 
+  def moderation_index
+    @batches = Batch.all
+    @accepted_variant_count = SubmittedVariant.where(status: 'accepted').count
+  end
+
+  def show_moderation
+    @batch = Batch.find_by(id: params[:id])
+  end
+
+  def update
+    @batch = Batch.find_by(id: params[:id])
+    message = params[:message].first
+    action = params[:variant_action]
+    variants = SubmittedVariant
+      .joins(:batch)
+      .where(id: params[:variant_ids], batches: { id: params[:id]})
+
+    variants.each do |variant|
+      variant.message = message
+      if action == 'reject'
+        variant.status = 'rejected'
+      elsif action == 'accept'
+        variant.status = 'accepted'
+      end
+      variant.save
+    end
+    render :show_moderation
+  end
+
   private
   def batch_params
-    params.permit(batch: [:name, :submitter_name, :submitter_email, :submitter_affiliation, :reason_for_inclusion, :file, :file_cache, :reference_sequence_version])
+    params.permit(batch: [:name, :submitter_name, :submitter_email, :submitter_affiliation, :reason_for_inclusion, :file, :file_cache, :tags, :reference_url])
   end
 end
