0a9b4842c9c35a156bd7a4eff02130e482bb6e2c
Adam Coffman <acoffman@genome.wustl.edu>
2016-05-18 16:38:54 -0500

    display meta tables generically instead of hardcoded as interactions

--- app/presenters/variant_overview_presenter.rb
+++ app/presenters/variant_overview_presenter.rb
@@ -65,20 +65,44 @@ class VariantOverviewPresenter < SimpleDelegator
     end
   end
 
-  def interaction_rows
-    drug_interactions.map do |di|
-      InteractionRow.new(
-        di.effect,
-        di.pathway,
-        di.clinical_association,
-        di.therapeutic_context,
-        di.status,
-        di.evidence,
-        source_link(di.source)
-      )
+  def meta_tables
+
+    if @meta_tables
+      @meta_tables
+    else
+      @meta_tables = []
+      all_tables = {}
+      disease_source_variants.each do |dsv|
+        if dsv.meta
+          tables = dsv.meta.values.map { |v| v['tables'] }.compact.flatten
+          tables.each do |table|
+            key = [table['name'], table['columns']]
+            (all_tables[key] ||= []).push(*table['rows'])
+          end
+        end
+      end
+      all_tables.each do |((name, columns), data)|
+        @meta_tables << TablePresenter.new(name, columns, data)
+      end
+    end
+  end
+
+  class TablePresenter
+    attr_reader :name, :columns, :data
+    def initialize(name, columns, data)
+      @data = data
+      @name = name
+      @columns = columns
+    end
+
+    def rows
+      data.map do |row|
+        columns.map do |col|
+          row[col]
+        end
+      end.uniq
     end
   end
 end
 
 DiseaseRow = Struct.new(:disease, :source, :batch, :tags, :external_links)
-InteractionRow = Struct.new(:effect, :pathway, :association, :drug, :status, :evidence_type, :source)
