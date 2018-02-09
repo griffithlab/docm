8e2bef90609323fbcd650034c47db443bdf4b02d
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-02-09 16:55:33 -0600

    Update configuration

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-server "ec2-18-220-192-136.us-east-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
+server "ec2-35-162-137-203.us-east-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
 
 set :rbenv_ruby, '2.3.4'
 
 set :ssh_options, {
-  keys: %w(/Users/acoffman/.ssh/acoffman-rails-testing.pem),
+  keys: [ENV['DOCM_PROD_KEY']],
   forward_agent: false,
   auth_methods: %w(publickey)
 }
