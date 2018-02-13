21e1a3e8d96086c4dc4a75384e946472e198ba64
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-02-13 14:15:36 -0600

    deploy file for staging server

---
+++ config/deploy/staging.rb
@@ -0,0 +1,10 @@
+server "ec2-52-34-91-132.us-west-2.compute.amazonaws.com", user: 'ubuntu', roles: %w{web}
+
+set :rbenv_ruby, '2.3.4'
+
+set :ssh_options, {
+  keys: [ENV['DOCM_STAGING_KEY']],
+  forward_agent: false,
+  auth_methods: %w(publickey)
+}
+
