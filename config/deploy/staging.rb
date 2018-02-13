09f8f483598763f821f729e5e1d4fa4e2e46aa44
Susanna Kiwala <ssiebert@genome.wustl.edu>
2018-02-13 14:22:36 -0600

    Set branch for staging deploy to staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,8 @@ server "ec2-52-34-91-132.us-west-2.compute.amazonaws.com", user: 'ubuntu', roles
 
 set :rbenv_ruby, '2.3.4'
 
+set :branch, 'staging'
+
 set :ssh_options, {
   keys: [ENV['DOCM_STAGING_KEY']],
   forward_agent: false,
